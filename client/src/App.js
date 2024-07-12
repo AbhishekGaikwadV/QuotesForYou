76ea16536cc0616c50af3129f02170cdc0f877d5
@abhishekgaikwad <abhishek.v.gaikwad@gmail.com>
2024-07-12 15:29:29 +0530

    Fixxed import statments in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,14 @@ import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Axios from 'axios';
 import Header from './Components/Header.component.jsx';
 import Footer from './Components/Footer.component.jsx';
-import Loader from './Components/Loader.component.jsx';
+import Loader from './Commponents/Loader.components.jsx';
 import Login from './Components/Login_Page_component.jsx';
-import SignUp from './Components/Sign_Up_Page.component.jsx'; // Fixed typo in import
+import SignUp from './Components/Sign_Up_Page.component.jsx';
 import ShowPage from './Components/ShowPage.component.jsx';
-import MainPage from './Components/MainPage.component.jsx'; // Fixed typo in import
+import MainPage from './Components/MainPage.component.jsx';
 import Quote_FTM from './Components/Quote_FTM.component.jsx';
 import Intuitive from './Components/Intuitive.component.jsx';
+
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import { toast, ToastContainer } from 'react-toastify';
